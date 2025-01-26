c2b640ad8e7a7f4fcf31fb419d343656ffa80a9e
Ariel <vilcheariel2@gmail.com>
2025-01-26 16:54:50 -0300

    problem with secret key unsolved

--- agency_API/src/main/java/com/travel/agency/service/UserService.java
+++ agency_API/src/main/java/com/travel/agency/service/UserService.java
@@ -3,9 +3,11 @@ package com.travel.agency.service;
 import com.travel.agency.enums.Role;
 import com.travel.agency.model.DTO.user.UserLoginDTO;
 import com.travel.agency.model.DTO.user.UserRegisterDTO;
+import com.travel.agency.model.DTO.user.UserDTO;
 import com.travel.agency.model.entities.User;
 import com.travel.agency.repository.UserRepository;
 import com.travel.agency.utils.JwtUtil;
+import com.travel.agency.utils.MapperUtil;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +15,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -66,15 +69,29 @@ public class UserService {
     //Verify method authenticates the User and creates a JWT TOKEN.
     //I need to create a custom exception error and handle it.
     public String verify(@Valid UserLoginDTO userLoginDTO) {
-        String username = Optional.ofNullable(userLoginDTO.username())
-                .orElse(userLoginDTO.email());
         try {
             authManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(username, userLoginDTO.password())
+                    new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password())
             );
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("User does not exists or credentials are wrong");
             }
-        return this.jwtUtil.generateToken(this.userDetailsService.loadUserByUsername(username));
+        return this.jwtUtil.generateToken(this.userDetailsService.loadUserByUsername(userLoginDTO.email()));
+    }
+
+    public List<UserDTO> users() {
+        List<User> users = this.userRepository.findAll();
+        return users.stream().map(user ->
+                MapperUtil.mapperEntity(user, userEntity ->
+                        new UserDTO(
+                                user.getId(),
+                                user.getFirstName(),
+                                user.getLastName(),
+                                user.getIdentityCard(),
+                                user.getEmail(),
+                                user.getUsername(),
+                                user.getPhoneNumber(),
+                                user.getRegisterDate()
+                        ))).toList();
     }
 }
