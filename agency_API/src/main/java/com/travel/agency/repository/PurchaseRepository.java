903e149fe073665691cfbc12b6f90b69fdb1747a
FlorEstrella <iniguezflorencia788@gmail.com>
2025-01-26 14:29:36 -0300

    terminando de limpiar codigp

--- agency_API/src/main/java/com/travel/agency/repository/PurchaseRepository.java
+++ agency_API/src/main/java/com/travel/agency/repository/PurchaseRepository.java
@@ -1,21 +1,8 @@
 package com.travel.agency.repository;
 
-import com.travel.agency.enums.Status;
 import com.travel.agency.model.entities.Purchase;
-import com.travel.agency.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
-public interface PurchaseRepository extends JpaRepository <Purchase,Long> {
-   // Optional<Purchase> findByUserIdAndStatus(Long userId, Status status);
-
-    List<Purchase> findByStatusAndPurchaseDateBefore(Status status, LocalDateTime timeoutLimit);
-
-    List<Purchase> findByStatus(Status status);
 }
