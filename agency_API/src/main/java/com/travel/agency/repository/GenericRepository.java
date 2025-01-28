d206be65304d1dc9ad44d3d2cd80df8726fca0f5
FlorIniguez <iniguezflorencia788@gmail.com>
2025-01-28 01:16:34 -0300

    arreglando error del mapper

--- agency_API/src/main/java/com/travel/agency/repository/GenericRepository.java
+++
@@ -1,4 +0,0 @@
-package com.travel.agency.repository;
-
-public interface GenericRepository {
-}
--- agency_API/src/main/java/com/travel/agency/repository/RatingRepository.java
+++ agency_API/src/main/java/com/travel/agency/repository/RatingRepository.java
@@ -1,6 +1,5 @@
 package com.travel.agency.repository;
 
-import com.travel.agency.model.DTO.rating.RatingDTO;
 import com.travel.agency.model.entities.Rating;
 import com.travel.agency.model.entities.TravelBundle;
 import com.travel.agency.model.entities.User;
@@ -10,7 +9,6 @@ import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     boolean existsByUserAndTravelBundle(User user, TravelBundle travelBundle);
-
 
     List<Rating> findByUser(User user);
 }
