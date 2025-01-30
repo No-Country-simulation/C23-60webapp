d937c2a4bce5aba0a9fdac5088f228e08dda4041
Ariel <vilcheariel2@gmail.com>
2025-01-30 19:35:30 -0300

    arreglando cosas

--- agency_API/src/main/java/com/travel/agency/controller/AuthController.java
+++ agency_API/src/main/java/com/travel/agency/controller/AuthController.java
@@ -4,7 +4,7 @@ package com.travel.agency.controller;
 import com.travel.agency.model.DTO.user.GeneratedTokenDTO;
 import com.travel.agency.model.DTO.user.UserLoginDTO;
 import com.travel.agency.model.DTO.user.UserRegisterDTO;
-import com.travel.agency.service.UserService;
+import com.travel.agency.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,22 +15,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
     @Autowired
-    public AuthController(UserService userService) {
-        this.userService = userService;
+    public AuthController(AuthService userService) {
+        this.authService = userService;
     }
 
     @PostMapping("/login")
         public ResponseEntity<GeneratedTokenDTO> login(@RequestBody @Valid UserLoginDTO userLoginDTO) {
-        String jwtToken = this.userService.verify(userLoginDTO);
+        String jwtToken = this.authService.verify(userLoginDTO);
         return ResponseEntity.status(HttpStatus.OK).body(new GeneratedTokenDTO(jwtToken));
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody @Valid UserRegisterDTO userRegisterDTO) {
-        this.userService.register(userRegisterDTO);
+        this.authService.register(userRegisterDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
--- agency_API/src/main/java/com/travel/agency/service/AuthService.java
+++ agency_API/src/main/java/com/travel/agency/service/AuthService.java
@@ -1,20 +1,48 @@
 package com.travel.agency.service;
 
+import com.travel.agency.enums.Role;
+import com.travel.agency.model.DTO.user.UserLoginDTO;
+import com.travel.agency.model.DTO.user.UserRegisterDTO;
 import com.travel.agency.model.entities.User;
 import com.travel.agency.repository.UserRepository;
+import com.travel.agency.utils.JwtUtil;
+import com.travel.agency.utils.MapperUtil;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authManager;
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+    private final ShoppingCartService ShoppingCartService;
 
-    public AuthService(UserRepository userRepository) {
+    @Autowired
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authManager, JwtUtil jwtUtil, CustomUserDetailsService userDetailsService, com.travel.agency.service.ShoppingCartService shoppingCartService) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authManager = authManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+        ShoppingCartService = shoppingCartService;
     }
 
     public User getUser() {
@@ -25,5 +53,34 @@ public class AuthService {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return userRepository.findByEmail(userDetails.getUsername())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + userDetails.getUsername()));
+    }
+
+    //Metodo registro chequea si el admin no esta registrado en db.
+    @Transactional
+    public void register(UserRegisterDTO userRegisterDTO) {
+        User user = MapperUtil.toEntity(userRegisterDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Optional<User> isAdminRegistered = userRepository.findByEmail("admin@gmail.com");
+        if(!isAdminRegistered.isPresent() && userRegisterDTO.email().equals("admin@gmail.com")){
+            user.setRoles(Set.of(Role.ADMIN));
+        }else {
+            user.setRoles(Set.of(Role.USER));
+        }
+        user.setRegisterDate(LocalDate.now());
+        this.userRepository
+                .save(user);
+        ShoppingCartService.createShoppingCart(user);
+    }
+
+    //Verify method authenticates the User and creates a JWT TOKEN.
+    public String verify(@Valid UserLoginDTO userLoginDTO) {
+        try {
+            authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password())
+            );
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("User does not exists or credentials are wrong");
+        }
+        return this.jwtUtil.generateToken(this.userDetailsService.loadUserByUsername(userLoginDTO.email()));
     }
 }
