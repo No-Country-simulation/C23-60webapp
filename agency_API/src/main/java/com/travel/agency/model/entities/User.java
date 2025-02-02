9976ab7057f80251e1d317a4992f16114acf5d5d
Ariel <vilcheariel2@gmail.com>
2025-02-02 20:58:11 -0300

    User fixed

--- agency_API/src/main/java/com/travel/agency/model/entities/User.java
+++ agency_API/src/main/java/com/travel/agency/model/entities/User.java
@@ -52,12 +52,13 @@ public class User implements UserDetails {
     private List<Purchase> purchases;
 
     
-    public User(String firstName, String lastName, Integer identityCard, String email, String username) {
+    public User(String firstName, String lastName, Integer identityCard, String email, String username, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.identityCard = identityCard;
         this.email = email;
         this.username = username;
+        this.password = password;
     }
 
     /**
